d3ced9173eb8eb5a449dfb7d63101f8ce3350cd4
Josh Lee <joshlee.dev@gmail.com>
2023-07-11 14:31:48 +0100

    first commit

---
+++ src/components/content/personal/Personal.js
@@ -0,0 +1,15 @@
+import './Personal.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Steven Clode
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
