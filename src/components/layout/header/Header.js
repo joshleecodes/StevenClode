b5de4fa20fa5451a6dcad81b0ec8b79490873945
Josh Lee <joshlee.dev@gmail.com>
2023-07-20 15:16:22 +0100

    implemented dynamic rendering on window resize and varied devices, implemented header section with navigation, design changes to body font to Montserrat

---
+++ src/components/layout/header/Header.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { HashLink as NavLink } from 'react-router-hash-link';
+import { scrollToTop, scrollTo } from './../../../util/scrollHelper';
+
+import SiteLogo from './../../../assets/imgs/svg/site-logo.svg';
+
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentSection: 'nav-landing',
+        }
+        this.handleActiveSection = this.handleActiveSection.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleActiveSection);
+        this.props.setHeaderOffSet();
+        this.props.setHeader();
+    }
+
+    handleActiveSection() {
+        
+       let offset = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+       let currentSection = this.state.currentSection;
+       let currentSectionClassList = document.getElementById(this.state.currentSection).classList;
+        {console.log(offset)}
+        if(offset <= 499) {
+            if(currentSection != "nav-landing") { currentSectionClassList.toggle("site-link-active"); }
+            currentSection = "nav-landing";
+        }
+        else if(offset >= 500 && offset < 1500 ) {
+            if(currentSection != "nav-landing") { currentSectionClassList.toggle("site-link-active"); }
+            document.getElementById("#nav-about").classList.add('site-link-active');
+            currentSection = "#nav-about";
+        }
+        else if(offset >= 1501 && offset < 2600) {
+            if(currentSection != "nav-landing") { currentSectionClassList.toggle("site-link-active"); }
+            document.getElementById("#nav-experience").classList.add('site-link-active');
+            currentSection = "#nav-experience";
+        }
+        else if(offset >= 2601 && offset < 4200) {
+            if(currentSection != "nav-landing") { currentSectionClassList.toggle("site-link-active"); }
+            document.getElementById("#nav-projects").classList.add('site-link-active');
+            currentSection = "#nav-projects";
+        }
+        else if(offset >= 4201) {
+            if(currentSection != "nav-landing") { currentSectionClassList.toggle("site-link-active"); }
+            document.getElementById("#nav-contact").classList.add('site-link-active');
+            currentSection = "#nav-contact";
+        }
+        this.setState({currentSection});
+    }
+
+    render() {
+        return (
+            <div id="header" className="header" data-aos="fade" data-aos-easing="ease-in-sine" data-aos-duration="800" data-aos-delay="800" data-aos-once="true" data-aos-anchor-placement="top-bottom">
+                <div className="header__container">
+                    <NavLink to="#landing-content" onClick={scrollToTop}>
+                        <img 
+                            className="header__logo" 
+                            id="nav-landing"
+                            onClick={this.onNavLinkClick}
+                            onMouseOver={this.props.handleIconHover} 
+                            onMouseLeave={this.props.handleIconHoverOff}  
+                            src={SiteLogo}
+                        />
+                    </NavLink>
+                    <div className="header__content-section">
+                        <div className="header__content-section__site-link-wrapper">
+                            <NavLink className="header__content-section__site-link" id="#nav-about" to="#about-content" onClick={scrollTo}>ABOUT</NavLink>
+                            <NavLink className="header__content-section__site-link" id="#nav-experience" to="#experience-content"  onClick={scrollTo}>EXPERIENCE</NavLink>
+                            <NavLink className="header__content-section__site-link" id="#nav-projects" to="#projects-content"  onClick={scrollTo}>PROJECTS</NavLink>                         
+                            <NavLink className="header__content-section__site-link" id="#nav-contact" to="#footer-content"  onClick={scrollTo}>CONTACT</NavLink>
+                        </div>
+                    </div>
+                </div>
+                <div className="header__border"></div>
+            </div>
+        );
+    }
+} 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import "@fontsource/karantina/300.css";
-import "@fontsource/karantina/400.css";
 import './index.scss';
 
+//routing imports
+import { createRoot } from 'react-dom/client';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+//animation library imports
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+//font imports
+import "@fontsource/karantina/300.css";
+import "@fontsource/karantina/400.css";
+import "@fontsource/montserrat";
 
 //component imports
 import MobileMenu from './components/layout/mobile-menu/Mobile-menu';
@@ -15,11 +21,54 @@ import About from './components/content/about/About';
 import Experience from './components/content/experience/Experience';
 import Projects from './components/content/projects/Projects';
 import Contact from './components/content/contact/Contact';
+import Header from './components/layout/header/Header';
 import Footer from './components/layout/footer/Footer';
 
+//initialise animation library
 AOS.init({
   mirror: true
 });
+
+
+
+//handle resize
+let isTablet = false;
+let isDesktop = false;
+let initialOffset;
+
+const handleResize = () => {
+  isDesktop = window.innerWidth >= 1280;
+  isTablet = window.innerWidth >= 900;
+  initialOffset = window.innerHeight;
+}
+
+window.onresize = handleResize();
+
+
+//Header
+let headerOffSet;
+let setHeaderOffSet = () => {
+    headerOffSet = document.getElementById("header").offsetTop;
+}
+
+let header;
+let setHeader = () => {
+    header = document.getElementById("header");
+}
+
+let handleStickyHeader = (initialOffSet) => {
+    if(window.pageYOffset >= initialOffSet) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+if(isDesktop == true){
+    window.onscroll = () => {
+        handleStickyHeader(headerOffSet);
+    }
+}
 
 
 //set route structure to be rendered
@@ -27,7 +76,16 @@ const routes = (
   <BrowserRouter>
     <div>
       <Landing/>
-      <MobileMenu/>
+      { isDesktop
+        ?
+        <Header
+          setHeaderOffSet={setHeaderOffSet}
+          setHeader={setHeader}
+        />
+        :
+        <MobileMenu/>
+      }
+      
       <About/>
       <Experience/>
       <Projects/>
@@ -36,7 +94,6 @@ const routes = (
     </div>
   </BrowserRouter>
 );
-
 
 //define root and render routes
 const root = createRoot(document.getElementById('root'));
